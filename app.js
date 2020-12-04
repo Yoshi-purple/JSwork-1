314237cbb460dde48098337f048e57c8c27804d1
Yoshi-purple <n7ydahew@gmail.com>
2020-12-04 17:26:58 +0900

    変数の削除

--- app.js
+++ app.js
@@ -2,19 +2,13 @@
 "use strict";
 
 {
-  let fizz = 0;
-  let buzz = 0;
-  let fizzbuzz = 0;
-  for (let i = 0; i < 99; i++) {
-    fizz++;
-    buzz++;
-    fizzbuzz++;
-    if (fizz % 15 === 0) {
-      console.log("FizzBuzz " + fizzbuzz);
-    } else if (buzz % 3 === 0) {
-      console.log("Fizz " + fizz);
-    } else if (buzz % 5 === 0) {
-      console.log("Buzz " + buzz);
+  for (let i = 1; i <= 99; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz " + i);
+    } else if (i % 3 === 0) {
+      console.log("Fizz " + i);
+    } else if (i % 5 === 0) {
+      console.log("Buzz " + i);
     }
   }
 }
