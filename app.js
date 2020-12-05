e46cec90e789a3f8ac3ee4598a5ad833284c1815
Yoshi-purple <n7ydahew@gmail.com>
2020-12-05 16:40:34 +0900

    条件の変更、テンプレートリテラル

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
 
 {
   for (let i = 1; i <= 99; i++) {
-    if (i % 15 === 0) {
-      console.log("FizzBuzz " + i);
+    if (i % 5 === 0 && i % 3 === 0) {
+      console.log(`FizzBuzz ${i}`);
     } else if (i % 3 === 0) {
-      console.log("Fizz " + i);
+      console.log(`Fizz ${i}`);
     } else if (i % 5 === 0) {
-      console.log("Buzz " + i);
+      console.log(`Buzz ${i}`);
     }
   }
 }
